d3a43aa830c8e8d5d4a34f34d6e61e9099e5ac51
Tim Bergsma <tim.bergsma@certara.com>
2017-02-18 10:27:01 -0500

    cran

--- R/csv.R
+++ R/csv.R
@@ -1,14 +1,22 @@
-#' Read or Write Standardized CSV
-
-#' Reads or writes CSV files in a standard way.
+#' Read or Write CSV Using Selected Conventions
+#' 
+#' Reads or writes CSV files in a conventional way. Generic, with methods for character and data.frame. A length-one character argument is treated as a filepath and tries to return a data.frame.  A data.frame argument is written to the specified filepath. Typically, \code{quote} and \code{row.names} are FALSE and \code{na} is ".".  When reading, white space and empty strings are treated as NA, and \code{strip.white} is TRUE.
+#'  
 #' @param x object
 #' @param ... passed arguments
+#' @seealso \code{\link{as.csv.character}}, \code{\link{as.csv.data.frame}}
 #' @export
+#' @examples
+#' data <- head(Theoph)
+#' filepath <- file.path(tempdir(),'theoph.csv')
+#' as.csv(data,filepath)
+#' as.csv(filepath)
+
 as.csv <- function(x,...)UseMethod('as.csv')
 
-#' Treat Character as CSV
+#' Treat Character as CSV filename.
 #' 
-#' Treat a character string as a csv filename.
+#' Treat a character string as a CSV filename.
 #' 
 #' If x is character, is length one, and is a path to a file, an attempt is made to read the file.  
 #' @inheritParams as.csv
@@ -17,16 +25,16 @@ as.csv <- function(x,...)UseMethod('as.csv')
 #' @param strip.white passed to read.csv
 #' @return data.frame
 #' @export
-as.csv.character <- function(x,as.is=TRUE,na.strings=c('',' ','.'),strip.white=T,...){
+as.csv.character <- function(x,as.is=TRUE,na.strings=c('','\\s','.'),strip.white=T,...){
   stopifnot(length(x)==1)
   stopifnot(file.exists(x))
   y <- utils::read.csv(x,as.is=as.is,na.strings=na.strings,strip.white=strip.white,...)
   y
 }
 
-#' Save a Data Frame as CSV
+#' Save a Data Frame as CSV.
 #' 
-#' Saves a data.frame as csv.
+#' Saves a data.frame as CSV, using selected conventions.
 #' 
 #' @inheritParams as.csv
 #' @param file passed to write.csv
